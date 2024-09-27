9ba02c265c45c012b7439a444104f5223881b66c
Jake <99535454+Jake1da3@users.noreply.github.com>
2024-09-27 21:13:10 +0000

    lols

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ print(board)
 
 """
 https://prod.liveshare.vsengsaas.visualstudio.com/join?2541150DE831BFBA0F1769B86FDDE9553B26
+https://prod.liveshare.vsengsaas.visualstudio.com/join?EB17AA86154D4C1A80CF9A401271FC5013DE
 
 """
