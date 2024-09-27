25357957b6750176210f02cdb76c80fdad95d323
Jake <99535454+Jake1da3@users.noreply.github.com>
2024-09-27 22:39:14 +0000

    a
    Co-authored-by: Elitragen2 <Elitragen2@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
-board_size = 3
-board = [[0 for _ in range(board_size)] for _ in range(board_size)]
+def makeBoard(optional = None): # makeBoard([1, 0, 0], [0, 2, 0], [0, 0, 3]])
+    board_size = 3
+    if optional == None:
+        board = [[0 for _ in range(board_size)] for _ in range(board_size)]
+    else:
+        board = optional
+
+    return board, board_size
+
+board, board_size = makeBoard([[1, 0, 2], [0, 2, 0], [3, 0, 3]]) # Global variables
+
+def printBoard():
+    for i in range(board_size):
+        print(str(board[i]).strip("[]"))
+
+printBoard()
 
 
-board[0][0] = 1
-print(board)
+def checkSum():
+    checkSumBool=False
+    exceptedSum = board_size * (1 + board_size) // 2  
+
+    for i in range(board_size):
+        row_sum = sum(board[i])
+        column_sum = sum([board[j][i] for j in range(board_size)])
+    
+    if exceptedSum == row_sum and exceptedSum == column_sum:
+        checkSumBool = True
+
+    return checkSumBool
+
+checkSum()
 
 """
-https://prod.liveshare.vsengsaas.visualstudio.com/join?2541150DE831BFBA0F1769B86FDDE9553B26
-https://prod.liveshare.vsengsaas.visualstudio.com/join?EB17AA86154D4C1A80CF9A401271FC5013DE
+
+[expression function]
+[board[j][i] for j in range(board_size)]
+[board[0][i], board[1][i]...]
+
+sum([board[0][i], board[1][i], board[2][i]])
+board[0][i], board[1][i], board[2][i]
+[[1, 0, 3], [1, 2, 0], [0, 0, 3]]
+
+
+  1,  0,  0 i=0
+  0,  2,  0 i=1
+  0,  0,  3 i=2
+j=0 j=1 j=2
 
 """
