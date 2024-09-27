ef12469452864d64eb9ff0bd0a32704500dd61df
Jake <99535454+Jake1da3@users.noreply.github.com>
2024-09-27 21:07:10 +0000

    aaaa

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ board = [[0 for _ in range(board_size)] for _ in range(board_size)]
 
 board[0][0] = 1
 print(board)
+
+"""
+https://prod.liveshare.vsengsaas.visualstudio.com/join?2541150DE831BFBA0F1769B86FDDE9553B26
+
+"""
